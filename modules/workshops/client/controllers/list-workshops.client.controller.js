cd10452a82e79baa852e88a1acf886f5c81aa4fe
ericgopak <eric.gopak@gmail.com>
2016-07-02 22:44:35 +0200

    Implemented sexy overlays, which catch mouse event and stop its propogation :) Those previews are actual YouTube video iframes, just blocked from clicking.

--- modules/workshops/client/controllers/list-workshops.client.controller.js
+++ modules/workshops/client/controllers/list-workshops.client.controller.js
@@ -1,35 +1,86 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('workshops')
-    .controller('WorkshopsListController', WorkshopsListController);
+    angular
+        .module('workshops')
+        .controller('WorkshopsListController', WorkshopsListController);
 
-  WorkshopsListController.$inject = ['WorkshopsService'];
+    WorkshopsListController.$inject = ['WorkshopsService'];
 
-  function WorkshopsListController(WorkshopsService) {
-    var vm = this;
+    function WorkshopsListController(WorkshopsService) {
+        var vm = this;
 
-    vm.workshops = WorkshopsService.query();
+        vm.workshops = WorkshopsService.query();
 
-    /*
-     https://www.youtube.com/watch?v=PH3HFEfXQ9Q
-     https://www.youtube.com/watch?v=PTbyvLGqTR4
-     https://www.youtube.com/watch?v=BV8LJOIMKqo
-     https://www.youtube.com/watch?v=_-kySh6jspg
-     https://www.youtube.com/watch?v=U0JAfqEak2c
-     https://www.youtube.com/watch?v=KxLKPQO3z1I
-     https://www.youtube.com/watch?v=M9JmC45VUtc
-     https://www.youtube.com/watch?v=mxOli8laZos
-     https://www.youtube.com/watch?v=E0D-30ynCBI
-     */
+        vm.videoClicked = function (str) {
+            console.log('Indeed! Clicked!', str);
+        };
 
-    vm.workshops = [{
-      'created': Date.now(),
-      'type': 'Online course',
-      'title': 'Creating CV for dummies',
-      'location': 'Munich',
-      'video_url': 'https://www.youtube.com/watch?v=PH3HFEfXQ9Q'
-    }];
-  }
+        vm.output = function (n) {
+            console.log(n, typeof n);
+        };
+
+        vm.getOverlayStyle = function (index) {
+            return {
+                'z-index': 1000,
+                height: '60px',
+                position: 'absolute',
+                left: index * 25 + '%'
+            };
+        };
+
+
+        /*
+         https://www.youtube.com/watch?v=PH3HFEfXQ9Q
+         https://www.youtube.com/watch?v=PTbyvLGqTR4
+         https://www.youtube.com/watch?v=BV8LJOIMKqo
+
+         https://www.youtube.com/watch?v=_-kySh6jspg
+
+         https://www.youtube.com/watch?v=U0JAfqEak2c
+         https://www.youtube.com/watch?v=KxLKPQO3z1I
+         https://www.youtube.com/watch?v=M9JmC45VUtc
+         https://www.youtube.com/watch?v=mxOli8laZos
+         https://www.youtube.com/watch?v=E0D-30ynCBI
+         */
+
+        vm.workshops = [{
+            'created': Date.now(),
+            'type': 'Online course',
+            'title': 'Creating CV for dummies',
+            'location': 'Munich',
+            'video_url': 'https://www.youtube.com/watch?v=PH3HFEfXQ9Q'
+        }, {
+            'created': Date.now(),
+            'type': 'Online course',
+            'title': 'Creating CV for dummies',
+            'location': 'Munich',
+            'video_url': 'https://www.youtube.com/watch?v=PTbyvLGqTR4'
+        }, {
+            'created': Date.now(),
+            'type': 'Online course',
+            'title': 'Creating CV for dummies',
+            'location': 'Munich',
+            'video_url': 'https://www.youtube.com/watch?v=BV8LJOIMKqo'
+        }, {
+            'created': Date.now(),
+            'type': 'Online course',
+            'title': 'Creating CV for dummies',
+            'location': 'Munich',
+            'video_url': 'https://www.youtube.com/watch?v=_-kySh6jspg'
+        }, {
+            'created': Date.now(),
+            'type': 'Online course',
+            'title': 'Creating CV for dummies',
+            'location': 'Munich',
+            'video_url': 'https://www.youtube.com/watch?v=U0JAfqEak2c'
+        }, {
+            'created': Date.now(),
+            'type': 'Online course',
+            'title': 'Creating CV for dummies',
+            'location': 'Munich',
+            'video_url': 'https://www.youtube.com/watch?v=PH3HFEfXQ9Q'
+        }
+        ];
+    }
 }());
