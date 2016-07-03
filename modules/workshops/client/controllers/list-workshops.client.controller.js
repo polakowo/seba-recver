692dee6e90e44c1c3ac1b330ee9805fd10150b2b
ericgopak <eric.gopak@gmail.com>
2016-07-03 18:10:15 +0200

    Switching between videos implemented.

--- modules/workshops/client/controllers/list-workshops.client.controller.js
+++ modules/workshops/client/controllers/list-workshops.client.controller.js
@@ -4,7 +4,7 @@
     angular
         .module('workshops')
         .controller('WorkshopsListController', WorkshopsListController)
-        .config(function(uiGmapGoogleMapApiProvider) {
+        .config(function (uiGmapGoogleMapApiProvider) {
             uiGmapGoogleMapApiProvider.configure({
                 key: 'AIzaSyCfPgzndLgMo_t0A8ikpCbZas4vCW-nHBg', // Generated on 02.07.2016
                 v: '3.20', //defaults to latest 3.X anyhow
@@ -18,34 +18,6 @@
         var vm = this;
 
         vm.workshops = WorkshopsService.query();
-
-        vm.videoClicked = function (str) {
-            console.log('Indeed! Clicked!', str);
-        };
-
-        vm.output = function (n) {
-            console.log(n, typeof n);
-        };
-
-        vm.getOverlayStyle = function (index) {
-            return {
-                'z-index': 1000,
-                height: '60px',
-                'background-color': 'yellow'
-            };
-            return {
-                'z-index': 1000,
-                height: '60px',
-                left: index * 25 + '%',
-                'background-color': 'yellow'
-            };
-        };
-
-        // Here's more on how to use it properly: http://angular-ui.github.io/angular-google-maps/#!/use
-        $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
-
-        vm.currentVideoURL = 'https://www.youtube.com/watch?v=PH3HFEfXQ9Q';
-
         vm.workshops = [{
             'created': Date.now(),
             'type': 'Online course',
@@ -101,5 +73,46 @@
             'location': 'Munich',
             'video_url': 'https://www.youtube.com/watch?v=E0D-30ynCBI'
         }];
+
+        vm.currentIndex = 0;
+
+        vm.videoClicked = function (index) {
+            console.log('Indeed! Clicked!', index, typeof index);
+            vm.currentIndex = index;
+        };
+
+        vm.prevClicked = function () {
+            if (vm.workshops.length > 0) {
+                vm.currentIndex--;
+                if (vm.currentIndex < 0) {
+                    vm.currentIndex = vm.workshops.length - 1;
+                }
+            }
+        };
+
+        vm.nextClicked = function () {
+            if (vm.workshops.length > 0) {
+                vm.currentIndex++;
+                if (vm.currentIndex >= vm.workshops.length) {
+                    vm.currentIndex = 0;
+                }
+            }
+        };
+
+        vm.output = function (n) {
+            console.log(n, typeof n);
+        };
+
+        vm.getOverlayStyle = function (index) {
+            return {
+                'z-index': 1000,
+                height: '60px'
+            };
+        };
+
+        // Here's more on how to use it properly: http://angular-ui.github.io/angular-google-maps/#!/use
+        $scope.map = {center: {latitude: 45, longitude: -73}, zoom: 8};
+
+        vm.currentVideoURL = 'https://www.youtube.com/watch?v=PH3HFEfXQ9Q';
     }
 }());
