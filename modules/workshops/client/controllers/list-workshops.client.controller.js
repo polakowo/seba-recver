2b2c23843eb835103d589bae2471ebeeaf84a815
ericgopak <eric.gopak@gmail.com>
2016-07-04 07:22:57 +0200

    Fixed navigation. Introduced 'Workshop' type of workshops

--- modules/workshops/client/controllers/list-workshops.client.controller.js
+++ modules/workshops/client/controllers/list-workshops.client.controller.js
@@ -7,7 +7,6 @@
         .config(function (uiGmapGoogleMapApiProvider) {
             uiGmapGoogleMapApiProvider.configure({
                 key: 'AIzaSyCfPgzndLgMo_t0A8ikpCbZas4vCW-nHBg', // Generated on 02.07.2016
-                v: '3.20', //defaults to latest 3.X anyhow
                 libraries: 'visualization'
             });
         });
@@ -17,8 +16,19 @@
     function WorkshopsListController(WorkshopsService, $scope) {
         var vm = this;
 
+        // http://www.slideshare.net/tariqhayat/cv-writing-workshop-12606160
+
+        // German CV writing
+        // https://www.ctp.org.uk/resettlement-courses/core-workshop/German+CV+writing
+
         vm.workshops = WorkshopsService.query();
         vm.workshops = [{
+            'created': Date.now(),
+            'type': 'Workshop',
+            'title': 'German CV writing',
+            'location': 'Munich',
+            'url': 'https://www.ctp.org.uk/resettlement-courses/core-workshop/German+CV+writing'
+        }, {
             'created': Date.now(),
             'type': 'Online course',
             'title': 'Creating CV for dummies',
@@ -76,24 +86,40 @@
 
         vm.currentIndex = 0;
 
+        vm.getOnlineCourses = function () {
+            return vm.workshops.filter(function (c) {
+                return c.type === 'Online course';
+            });
+        };
+
+        vm.getOnlineCourse = function (index) {
+            var courses = vm.getOnlineCourses();
+            if (index >= 0 && index < courses.length) {
+                return courses[index];
+            }
+            return courses[courses.length > 0 ? courses.length - 1 : 0];
+        };
+
         vm.videoClicked = function (index) {
             console.log('Indeed! Clicked!', index, typeof index);
             vm.currentIndex = index;
         };
 
         vm.prevClicked = function () {
-            if (vm.workshops.length > 0) {
+            var courses = vm.getOnlineCourses();
+            if (courses.length > 0) {
                 vm.currentIndex--;
                 if (vm.currentIndex < 0) {
-                    vm.currentIndex = vm.workshops.length - 1;
+                    vm.currentIndex = courses.length - 1;
                 }
             }
         };
 
         vm.nextClicked = function () {
-            if (vm.workshops.length > 0) {
+            var courses = vm.getOnlineCourses();
+            if (courses.length > 0) {
                 vm.currentIndex++;
-                if (vm.currentIndex >= vm.workshops.length) {
+                if (vm.currentIndex >= courses.length) {
                     vm.currentIndex = 0;
                 }
             }
@@ -111,7 +137,32 @@
         };
 
         // Here's more on how to use it properly: http://angular-ui.github.io/angular-google-maps/#!/use
-        $scope.map = {center: {latitude: 45, longitude: -73}, zoom: 8};
+        vm.map = {center: {latitude: 45, longitude: -73}, zoom: 8};
+
+        vm.markers = [{
+            id: 0,
+            coords: {
+                latitude: 45,
+                longitude: -73
+            },
+            options: {draggable: true}
+            // events: {
+            //     dragend: function (marker, eventName, args) {
+            //         $log.log('marker dragend');
+            //         var lat = marker.getPosition().lat();
+            //         var lon = marker.getPosition().lng();
+            //         $log.log(lat);
+            //         $log.log(lon);
+            //
+            //         $scope.marker.options = {
+            //             draggable: true,
+            //             labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
+            //             labelAnchor: "100 0",
+            //             labelClass: "marker-labels"
+            //         };
+            //     }
+            // }
+        }];
 
         vm.currentVideoURL = 'https://www.youtube.com/watch?v=PH3HFEfXQ9Q';
     }
